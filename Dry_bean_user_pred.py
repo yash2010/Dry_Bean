ed69859e962fb607cc4ed3d71bf44d55dc087c9b
yash2010 <128846477+yash2010@users.noreply.github.com>
2023-03-24 23:30:49 +0100

    Update Dry_bean_user_pred.py

--- Dry_bean_user_pred.py
+++ Dry_bean_user_pred.py
@@ -1,22 +1,48 @@
 import pandas as pd
 import numpy as np
+from keras.callbacks import TensorBoard 
 from sklearn.cluster import KMeans
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# load and preprocess the data
+df_original = pd.read_excel("Dry_Bean_Dataset.xlsx", sheet_name=0)
 df = pd.read_excel("Dry_Bean_Dataset.xlsx", sheet_name=0).drop("Class", axis=1)
+print(df.head)
 cols = df.columns
+
+for i in range(len(cols)-1):
+  for j in range(i+1, len(cols)-1):
+    x_label = cols[i]
+    y_label= cols[j]
+    sns.scatterplot(x=x_label, y=y_label, data=df, hue="Class")
+    plt.title("Original Data point")
+    plt.show()
+
 X = df[cols].values
-
-# train KMeans model
 kmeans = KMeans(n_clusters=6).fit(X)
+cluster_df = pd.DataFrame(np.hstack((X, kmeans.labels_.reshape(-1,1))), columns = cols.tolist()+["Cluster"])
 
-# ask user to enter feature values for new data
+print(cluster_df["Cluster"].value_counts())
+
+for i in range(len(cols)-1):
+    for j in range(i+1, len(cols)):
+        x_label = cols[i]
+        y_label = cols[j]
+        sns.scatterplot(x=x_label, y=y_label, data=cluster_df, hue='Cluster')
+        plt.title("Clustered")
+        plt.show()
+
+
 new_data = []
 for col in cols:
     val = float(input(f"Enter {col}: "))
     new_data.append(val)
 
-# predict the cluster for new data
+
 new_data_cluster = kmeans.predict(np.array(new_data).reshape(1, -1))
 
+
 print(f"The predicted cluster for the new data point is {new_data_cluster[0]}")
+
+
+
